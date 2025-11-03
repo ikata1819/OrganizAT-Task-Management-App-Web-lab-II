af5f39d4497c89d4eb21ea6497168b5fcb4a4353
Anika Tasnim <anikatasnim1066@gmail.com>
2025-11-03 20:56:43 +0600

    Submit lab assigment 2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,58 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 const tasks = [
-       { id: 1, title: 'Learn Node.js', completed: false },
-       { id: 2, title: 'Build REST API', completed: false }
+  {
+    id: 1,
+    title: "Install Node.js and npm",
+    completed: false,
+    priority: "Low",
+    createdAt: new Date(),
+  },
+  {
+    id: 2,
+    title: "Set up an Express server",
+    completed: false,
+    priority: "Low",
+    createdAt: new Date(),
+  },
+  {
+    id: 3,
+    title: "Create basic routes",
+    completed: false,
+    priority: "Medium",
+    createdAt: new Date(),
+  },
+  {
+    id: 4,
+    title: "Implement a basic GET tasks route",
+    completed: false,
+    priority: "Medium",
+    createdAt: new Date(),
+  },
+  {
+    id: 5,
+    title: "Test the route using Postman",
+    completed: false,
+    priority: "High",
+    createdAt: new Date(),
+  },
 ];
-app.get('/', (req, res) => {
-       res.send('Task Management API is running!');
+
+const health = [
+  { "status": "healthy", "uptime": process.uptime()}
+];
+
+
+app.get("/", (req, res) => {
+  res.send("Task Management API is running!");
 });
-app.get('/tasks', (req, res) => {
-       res.json(tasks);
+app.get("/tasks", (req, res) => {
+  res.json(tasks);
+});
+app.get("/health", (req, res) => {
+  res.json(health);
 });
 app.listen(port, () => {
-       console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
