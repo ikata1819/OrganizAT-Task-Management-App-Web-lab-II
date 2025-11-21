4d150fc6506f7f19657575068262936131fc9182
Anika Tasnim <anikatasnim1066@gmail.com>
2025-11-22 01:04:57 +0600

    Submit Lab3:Mysql files

--- src/index.js
+++ src/index.js
@@ -2,27 +2,6 @@ const express = require('express');
 const taskRouter = require('./routes/tasks');
 const app = express();
 app.use(express.json());
-const tasks = [
-  {
-    id: 1,
-    title: "Install Node.js and npm",
-    completed: false,
-  },
-  {
-    id: 2,
-    title: "Set up an Express server",
-    completed: false,
-  },
-  {
-    id: 3,
-    title: "Create basic routes",
-    completed: false,
-  },
-  
-];
-
-app.locals.tasks = tasks; 
-
 
 app.use('/tasks', taskRouter);
 
