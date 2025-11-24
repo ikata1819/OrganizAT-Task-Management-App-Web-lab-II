d91bade23b5c94ebd583c585bc4efb44e80e0ac2
Anika Tasnim <anikatasnim1066@gmail.com>
2025-11-24 21:17:29 +0600

    submit lab3 extra assignment: creating a .env file to configure database in a secure way

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,16 @@
+
 const mysql = require('mysql2');
+
 const connection = mysql.createPool({
-host: 'localhost',
-user: 'taskuser',
-password: 'taskpass123',
-database: 'organizat',
-waitForConnections: true,
-connectionLimit: 10,
-queueLimit: 0
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT) || 10,
+  queueLimit: 0
 });
+
+
 const promiseConnection = connection.promise();
 module.exports = promiseConnection;
